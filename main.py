2b1f091de1eb33351a6141e0ce9b2c7df0183b8c
Sean Chan <sean.chan3@student.education.wa.edu.au>
2025-03-29 18:55:04 +0800

    changed the input system a bit so that the user can enter the value and time unit into one line did this by using a new function called inp_tf()     gets user input with user error and splits it into 2 parts, time and unit     returns both as 2 different variables

--- main.py
+++ main.py
@@ -1,22 +1,36 @@
-def convert_time(fr:str, fr_amount:int, to:str):
-    seconds = {"d": 86400, "hd": 86400/2, "qd": 86400/4, "h": 3600, "m": 60, "s": 1}
-    return (seconds[fr.lower()] / seconds[to.lower()]) * fr_amount
+def conv_time(fr:str, fr_amt:int, to:str):
+    sec = {"d": 86400, "hd": 86400/2, "qd": 86400/4, "h": 3600, "m": 60, "s": 1}
+    return (sec[fr.lower()] / sec[to.lower()]) * fr_amt
 
-def pop_size(initial_population:int, growth_rate:float, gr_time_unit:str, projection_time:int, pt_unit:str, fission_rate:float = -1) -> float:
-    growth_rate = convert_time(gr_time_unit, growth_rate, pt_unit)
-    if fission_rate == -1:
-        return initial_population * (1 + (growth_rate / 100) * projection_time)
+def pop_sz(init_pop:int, gr_rate:float, gr_tu:str, proj_time:int, pt_tu:str, fiss_rate:float = -1):
+    gr_rate = conv_time(gr_tu, gr_rate, pt_tu)
+    if fiss_rate == -1:
+        return init_pop * (1 + (gr_rate / 100) * proj_time)
     else:
         pass
 
+def inp_tf(prompt:str):
+    while True:
+        try:
+            inp = input(prompt).split()
+            a = inp[0]
+            u = inp[1]
+            if u not in ["d", "hd", "qd", "h", "m", "s"]:
+                print("Use correct unit. (d, hd, qd, h, m, s)")
+                continue
+            a = int(a)
+            return a, u
+        except IndexError:
+            print("Enter in format 'number<space>unit'.")
+        except ValueError:
+            print("First value must be a number.")
+
 if __name__ == "__main__":
-    initial_population = input("Enter the initial population: ")
-    growth_rate = input("Enter the growth rate (%): ")
-    gr_time_unit = input("Enter the growth rate time unit (d, hd, qd, h, m, s): ")
+    init_pop = input("Enter the initial population: ")
+    gr_rate = inp_tf("Enter the growth rate (%) and its time unit (d, hd, qd, h, m, s): ")
 
     print("Future projection timeframe:")
-    project_time = input("Enter the project time: ")
-    pr_time_unit = input("Enter the project time unit (d, hd, qd, h, m, s): ")
+    proj_time = inp_tf("Enter the project time and its time unit (d, hd, qd, h, m, s): ")
 
-    result = pop_size(initial_population, growth_rate, gr_time_unit, project_time, pr_time_unit)
-    print(f"Population after {project_time} {pr_time_unit}: {result}")
+    result = pop_sz(init_pop, gr_rate, gr_tu, proj_time, pt_tu)
+    print(f"Population after {proj_time} {pt_tu}: {result}")
