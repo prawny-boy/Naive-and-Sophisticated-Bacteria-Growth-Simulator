3e8b2927da2e58a1aba5bac9ef06968faf1e7e76
Sean Chan <sean.chan3@student.education.wa.edu.au>
2025-03-29 18:59:07 +0800

    Decided to switch to using lists with 2 values this is replacing the 2 variables     for growth rate, projection and fission rate edited the pop_sz() function to use the lists fixed the issue where i forgot to plug in the right variables into the pop_sz() function

--- main.py
+++ main.py
@@ -2,10 +2,12 @@ def conv_time(fr:str, fr_amt:int, to:str):
     sec = {"d": 86400, "hd": 86400/2, "qd": 86400/4, "h": 3600, "m": 60, "s": 1}
     return (sec[fr.lower()] / sec[to.lower()]) * fr_amt
 
-def pop_sz(init_pop:int, gr_rate:float, gr_tu:str, proj_time:int, pt_tu:str, fiss_rate:float = -1):
+def pop_sz(init_pop:float, gr_rate:list[float, str], proj_time:list[float, str], fiss_rate:list[float, str] = -1):
+    gr_rate, gr_tu = gr_rate
+    proj_time, pt_tu = proj_time
     gr_rate = conv_time(gr_tu, gr_rate, pt_tu)
     if fiss_rate == -1:
-        return init_pop * (1 + (gr_rate / 100) * proj_time)
+        return round(float(init_pop) * (1 + (gr_rate / 100) * proj_time))
     else:
         pass
 
@@ -18,12 +20,13 @@ def inp_tf(prompt:str):
             if u not in ["d", "hd", "qd", "h", "m", "s"]:
                 print("Use correct unit. (d, hd, qd, h, m, s)")
                 continue
-            a = int(a)
-            return a, u
+            a = float(a)
+            return [a, u]
         except IndexError:
             print("Enter in format 'number<space>unit'.")
         except ValueError:
             print("First value must be a number.")
+
 
 if __name__ == "__main__":
     init_pop = input("Enter the initial population: ")
@@ -32,5 +35,5 @@ if __name__ == "__main__":
     print("Future projection timeframe:")
     proj_time = inp_tf("Enter the project time and its time unit (d, hd, qd, h, m, s): ")
 
-    result = pop_sz(init_pop, gr_rate, gr_tu, proj_time, pt_tu)
-    print(f"Population after {proj_time} {pt_tu}: {result}")
+    result = pop_sz(init_pop, gr_rate, proj_time)
+    print(f"Population after {proj_time[0]} {proj_time[1]}: {result}")
