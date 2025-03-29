b409848d44187d1edddec0c83a3d7b0f14f5954a
Sean Chan <sean.chan3@student.education.wa.edu.au>
2025-03-29 18:54:01 +0800

    Started implementing a linear bacteria growth and some inputting for testing

--- main.py
+++ main.py
@@ -1,3 +1,22 @@
-def conv_time(fr:str, fr_amt:int, to:str):
-    sec = {"d": 86400, "hd": 86400/2, "qd": 86400/4, "h": 3600, "m": 60, "s": 1}
-    return (sec[fr.lower()] / sec[to.lower()]) * fr_amt
+def convert_time(fr:str, fr_amount:int, to:str):
+    seconds = {"d": 86400, "hd": 86400/2, "qd": 86400/4, "h": 3600, "m": 60, "s": 1}
+    return (seconds[fr.lower()] / seconds[to.lower()]) * fr_amount
+
+def pop_size(initial_population:int, growth_rate:float, gr_time_unit:str, projection_time:int, pt_unit:str, fission_rate:float = -1) -> float:
+    growth_rate = convert_time(gr_time_unit, growth_rate, pt_unit)
+    if fission_rate == -1:
+        return initial_population * (1 + (growth_rate / 100) * projection_time)
+    else:
+        pass
+
+if __name__ == "__main__":
+    initial_population = input("Enter the initial population: ")
+    growth_rate = input("Enter the growth rate (%): ")
+    gr_time_unit = input("Enter the growth rate time unit (d, hd, qd, h, m, s): ")
+
+    print("Future projection timeframe:")
+    project_time = input("Enter the project time: ")
+    pr_time_unit = input("Enter the project time unit (d, hd, qd, h, m, s): ")
+
+    result = pop_size(initial_population, growth_rate, gr_time_unit, project_time, pr_time_unit)
+    print(f"Population after {project_time} {pr_time_unit}: {result}")
